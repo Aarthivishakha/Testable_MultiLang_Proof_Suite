5865b58b2b4a5569fc142da308c018acb0994135
Aarthivishakha <aarthivishakha@gmail.com>
2026-08-18 14:44:49 +0530

    javascript: remove Python mitmproxy addon

--- compliance/mitmproxy/pii_check.py
+++
@@ -1,7 +0,0 @@
-from mitmproxy import http
-
-
-def response(flow: http.HTTPFlow) -> None:
-    text = flow.response.get_text(strict=False)
-    if "student@example.invalid" in text:
-        flow.response.headers["x-testable-pii-fixture"] = "detected"
